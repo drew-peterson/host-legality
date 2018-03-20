451378fe22f8ee2fdcab117bff92e063d2705afc
weeznog <drew.m.peterson@gmail.com>
2018-03-20 13:47:52 -0700

    update ci to use MONGO_URL var

--- config/ci.js
+++ config/ci.js
@@ -4,12 +4,12 @@ module.exports = {
   googleClientSecret: 'B27inwogWR1DffLrreksJz0-',
   FACEBOOK_APP_ID: '173102873298551',
   FACEBOOK_APP_SECRET: 'fa096f8524f34486de4e8c3ba6f990d7',
-  mongoURI: 'mongodb://127.0.0.1:27017/host_legality_ci',
+  MONGO_URI: 'mongodb://127.0.0.1:27017/host_legality_ci',
   COOKIE_KEY: 'jasdfjkawerjwerhrkejkdkxjhzsxhdwerwxddddsdfjsdlfkjfjskw',
   STRIPE_PUBLISHABLE_KEY: 'pk_test_ZRQpB7B9vIQFhYozuoUbjVYW',
   STRIPE_SECRET_KEY: 'sk_test_EKDSMMLJbQwyZTitGCwRcCzl',
   SENDGRID_KEY:
     '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',
   REDIRECT_DOMAIN: 'http://localhost:3000',
-  redisUrl: 'redis://127.0.0.1:6379' // default port for redis
+  REDIS_URL: 'redis://127.0.0.1:6379' // default port for redis
 };
